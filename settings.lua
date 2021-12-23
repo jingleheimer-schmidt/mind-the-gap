e293c43977c058074e126b92c0bee908bf0a2d1a
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2021-12-23 13:40:52 -0800

    0.0.2

--- settings.lua
+++ settings.lua
@@ -1,10 +1,26 @@
+--
+-- local updateFrequency = {
+--   type = "int-setting",
+--   name = "void-update-frequency",
+--   setting_type = "runtime-global",
+--   default_value = 60,
+--   minimum_value = 1,
+-- }
 
 local updateFrequency = {
-  type = "int-setting",
+  type = "string-setting",
   name = "void-update-frequency",
   setting_type = "runtime-global",
-  default_value = 60,
-  minimum_value = 1,
+  default_value = "10-ticks",
+  allowed_values = {
+    "disabled",
+    "10-ticks",
+    "30-ticks",
+    "60-ticks",
+    "120-ticks",
+    "240-ticks",
+    "480-ticks"
+  }
 }
 
 data:extend({
