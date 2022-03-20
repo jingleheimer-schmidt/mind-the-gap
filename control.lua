e541cd90cacf88863b451bb29627e29ecd08d0c5
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2022-03-20 16:56:32 -0700

    0.0.3

--- control.lua
+++ control.lua
@@ -1,10 +1,19 @@
 
 script.on_event(defines.events.on_player_changed_position, function(event)
   local player = game.get_player(event.player_index)
+  local bounding_box = {}
+  if player.character then
+    bounding_box = player.character.bounding_box
+  elseif player.vehicle then
+    bounding_box = player.vehicle.bounding_box
+  else
+    return
+  end
   local tile = player.surface.find_tiles_filtered(
   {
-    position = player.position,
-    radius = 1,
+    -- position = player.position,
+    -- radius = 1,
+    area = bounding_box,
     name = "out-of-map",
     limit = 1,
   })
@@ -39,21 +48,16 @@ script.on_nth_tick(10, function()
   elseif global.counter < update_frequencies[frequency] then
     global.counter = global.counter + 1
   else
-    for a,b in pairs(game.surfaces) do
-      local vehicles = b.find_entities_filtered(
-      {
-        type = {"car", "spider-vehicle"}
-      })
-      for c,d in pairs(vehicles) do
-        local tile = d.surface.find_tiles_filtered(
-        {
-          position = d.position,
-          radius = 1.2,
-          name = "out-of-map",
-          limit = 1,
+    for unit_number, vehicle in pairs(global.vehicles) do
+      if not vehicle.valid then
+        global.vehicles[unit_number] = nil
+      else
+        local tile = vehicle.surface.find_tiles_filtered({
+          area = vehicle.bounding_box,
+          name = "out-of-map"
         })
-        if tile[1] and tile[1].name == "out-of-map" then
-          d.die()
+        if tile[1] then
+          vehicle.die()
         end
       end
     end
@@ -61,3 +65,42 @@ script.on_nth_tick(10, function()
   end
 end
 )
+
+if not global.vehicles then
+  global.vehicles = {}
+end
+
+script.on_init(function()
+  for every, surface in pairs(game.surfaces) do
+    for each, vehicle in pairs(surface.find_entities_filtered{type={"car","spider-vehicle"}}) do
+      if not global.vehicles then
+        global.vehicles = {}
+      end
+      global.vehicles[vehicle.unit_number] = vehicle
+    end
+  end
+end)
+
+script.on_configuration_changed(function()
+  for every, surface in pairs(game.surfaces) do
+    for each, vehicle in pairs(surface.find_entities_filtered{type={"car","spider-vehicle"}}) do
+      if not global.vehicles then
+        global.vehicles = {}
+      end
+      global.vehicles[vehicle.unit_number] = vehicle
+    end
+  end
+end)
+
+function on_built(event)
+  local entity = event.created_entity or event.entity or event.destination
+  if entity.type == "car" or entity.type == "spider-vehicle" then
+    global.vehicles[entity.unit_number] = entity
+  end
+end
+
+script.on_event(defines.events.on_built_entity, on_built)
+script.on_event(defines.events.on_entity_cloned, on_built)
+script.on_event(defines.events.on_robot_built_entity, on_built)
+script.on_event(defines.events.script_raised_built, on_built)
+script.on_event(defines.events.script_raised_revive, on_built)
